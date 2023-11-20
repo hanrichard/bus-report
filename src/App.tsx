7ac2f46945fca5dac5a9f45feae7fd870529385d
richardhan <hxy.314@gmail.com>
2023-11-20 22:59:15 +1100

    Add mui lib

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css'
+import Container from "@material-ui/core/Container";
 
 const App = () => {
   return (
-    <>
+    <Container>
       this is app
-    </>
+    </Container>
   )
 }
 
